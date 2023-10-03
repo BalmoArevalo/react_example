97a88b72bb43da1302d7f7b3850bc6ae0bb79663
Emerson Arevalo <AO22002@ues.edu.sv>
2023-10-03 08:51:09 -0600

    Add link rel to examples and import Example1

---
+++ app/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const Footer = () => {
+    return (
+        <div className='w-full px-6 py-12 bg-blue-500 flex flex-row
+justify-center text-white'>
+            <a className='hover:underline' href='mailto:ao22002@ues.edu.sv'>Emerson Arévalo - 2023</a>
+        </div>
+    )
+}
+export default Footer;
---
+++ app/pages/examples/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Example1 from './1/page'
+const ExamplesHome = () => {
+return (
+<div>
+    <h1 className='text-2xl font-bold'>Ejemplos</h1>
+    <p>Esta seccion del sitio web esta destinada a alojar ejemplos en react.</p>
+    <Example1/>
+</div>
+)
+}
+export default ExamplesHome
