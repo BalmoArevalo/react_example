347bd732221a042d2e74516d8fd4f1d4628a338b
Emerson Arevalo <AO22002@ues.edu.sv>
2023-10-03 11:14:35 -0600

    add file utils and add constants in order to creat an enum for linkRoutes

--- app/pages/examples/layout.tsx
+++ app/pages/examples/layout.tsx
@@ -1,5 +1,9 @@
 import Link from 'next/link'
 import React, { ReactNode } from 'react'
+import LinkRoutes from '@/utils/constants';
+import { linkSync } from 'fs';
+
+
 
 interface IExampleLayoutProps {
     children: ReactNode
@@ -9,22 +13,22 @@ const ExamplesLayout = ({children}:IExampleLayoutProps) =>{
     return(
         <div className='flex flex-row gap-4'>
             <div className='border border-gray-300 rounded-md h-fit self-start overflow-hidden'>
-                <Link href="/pages/examples">
+                <Link href={LinkRoutes.home}>
                     <div className='p-2 hover:bg-blue-400 hover:text-white bordder-b border-gray-300'>
                         Inicio
                     </div>
                 </Link>
-                <Link href="/pages/examples/1">
+                <Link href={LinkRoutes.example1}>
                     <div className='p-2 hover:bg-blue-400 hover:text-white bordder-b border-gray-300'>
                         Ejemplo 1: Renderizar lista de objetos
                     </div>
                 </Link>
-                <Link href="/pages/examples/2">
+                <Link href={LinkRoutes.example2}>
                     <div className='p-2 hover:bg-blue-400 hover:text-white bordder-b border-gray-300'>
                         Ejemplo 2: Capturar datos y almacenar en estado                   
                     </div>
                 </Link>
-                <Link href="/pages/examples/3">
+                <Link href={LinkRoutes.example3}>
                     <div className='p-2 hover:bg-blue-400 hover:text-white bordder-b border-gray-300'>
                         Ejemplo 3: Renderizar lista de un estado y manipular el estado                   
                     </div>
---
+++ utils/constants.ts
@@ -0,0 +1,10 @@
+import { constants } from "buffer";
+
+enum LinkRoutes{
+    home = '/pages/examples',
+    example1 = "/pages/examples/1",
+    example2 = "/pages/examples/2",
+    example3 = "/pages/examples/3",
+}
+
+export default LinkRoutes
