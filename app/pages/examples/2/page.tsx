9f874636555165732899cf36a12d37af6da8876b
Emerson Arevalo <AO22002@ues.edu.sv>
2023-10-03 16:45:32 -0600

    Add input to put an image

--- app/pages/examples/2/page.tsx
+++ app/pages/examples/2/page.tsx
@@ -1,11 +1,18 @@
 'use client';
 import React, { ChangeEvent, useState }  from "react";
-
+import { UserCardProps } from "@/app/components/UserCard";
 type TRoles = 'Client'|'Admin';
 
+interface IUser extends UserCardProps {
+    id: number;
+    picture: string;
+  }
+
 const Example2 = ()=>{
+    const [users, setUsers] = useState<IUser[]>([]);
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
+    const [picture, setPicture] = useState('');
     const [role, setRole] = useState<TRoles>('Client')
     const [terms, setTerms] = useState(false);
 
@@ -16,6 +23,10 @@ const Example2 = ()=>{
     const handleLastNameOnChange = (event:ChangeEvent<HTMLInputElement>)=>{
         setLastName(event.currentTarget.value);
     }
+
+    const handlePictureOnChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setPicture(event.currentTarget.value);
+      };
 
     const handleRoleOnChange = (event:ChangeEvent<HTMLSelectElement>)=>{
         if(event.currentTarget.value === 'Client' || event.currentTarget.value === 'Admin'){
@@ -32,6 +43,7 @@ const Example2 = ()=>{
         console.log('--------------')
         console.log('Nombre:', firstName)
         console.log('Apellido:', lastName)
+        console.log('Picture:', picture)
         console.log('Rol:', role)
         console.log(terms ? 'Acepto los terminos' : 'No acepto los terminos')
         console.log('--------------')
@@ -45,6 +57,10 @@ const Example2 = ()=>{
             <div className="flex flex-col">
                 <label htmlFor="lastName">Apellido: </label>
                 <input id='lastName'  type="text" onChange={handleLastNameOnChange} value={lastName} />
+            </div>
+            <div className="flex flex-col">
+                <label htmlFor="picture">Imagen: </label>
+                <input id='picture' type="text" onChange={handlePictureOnChange} value={picture} />
             </div>
             <div className="flex flex-col">
                 <label htmlFor="rol">Rol: </label>
